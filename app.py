201af785755ce27dad7a6fd0ffa6dd286ef1d3b1
Anmol Ranjan <129182761+anmolranjan045@users.noreply.github.com>
2024-07-10 11:10:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def main():
 
     # @st.cache_data(persist=True)
     def load_data():
-        data = pd.read_csv("C:/Users/anmol/Documents/Streamlit ML Models/mushrooms.csv")
+        data = pd.read_csv("mushrooms.csv")
         label = LabelEncoder()
         for col in data.columns:
             data[col] = label.fit_transform(data[col])
